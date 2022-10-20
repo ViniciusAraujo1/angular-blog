d6ddbdf3dfcc1f1a846593c996c7c8f6e72713f2
ViniciusAraujo1 <viniciusaraujoaj@gmail.com>
2022-10-20 10:33:26 -0300

    content created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { ContentComponent } from './pages/content/content.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'content/:id',
+    component: ContentComponent
+  }
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+
+})
+export class AppRoutingModule { }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novo Homem Aranha anunciado",
+    "description": "Filme ja tem data de lancamento.",
+    "photoCover": "https://cdn.pocket-lint.com/r/s/x1920/assets/images/152053-tv-feature-what-is-the-best-order-to-watch-all-the-spider-man-movies-and-shows-image1-lpdrxizmfk.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Vaza novo e incrível uniforme do Demolidor na próxima série da Marvel",
+    "description": "Novas fotos surgiram na internet",
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2022/10/Vaza-novo-e-incrivel-uniforme-do-Demolidor-na-proxima-serie-da-Marvel-legadodamarvel-750x394.webp"
+  }
+
+]
+
